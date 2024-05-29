c971dea2d654dc576dd364b5f45940d6f4dc22b3
hrsvrn <harshvardhanvatsa@gmail.com>
2024-05-29 21:34:53 +0530

    appended main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,7 @@ export default function Home() {
     <main className="text-center pt-32 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">Welcome to My Blog</h1>
       <p className="max-w-[1500px] mx-auto leading-8">
-        Lorem ipsum dolor sit amet co nsectetur adipisicing elit. Illum, pariatur sunt! Quis totam, tenetur expedita non voluptatibus officia ab maiores consequuntur fugit est, et inventore! Porro neque molestiae rem sequi totam repellat tempore sapiente accusantium sint. Nesciunt accusamus voluptatem incidunt?
-      </p>
+      Currently an UnderGrad student at VIT Vellore. I have a deep interest in DevOps,Machine Learning and Back-End Developement. I am open to help anyone with any projects and do collaborations!     </p>
     </main>
   );
 }
