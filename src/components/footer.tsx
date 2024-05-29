397025ddb6b884075b2e0003a0e4c3709da6e1f6
hrsvrn <harshvardhanvatsa@gmail.com>
2024-05-29 22:31:43 +0530

    added linkedin url to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="text-center mt-auto text-zinc-400 py-5 px-7 border-t"  >&copy; 2024 Harshvardhan Vatsa (hrsvrn)
+    <footer className="text-center mt-auto text-zinc-400 py-5 px-7 border-t"  >&copy; <a href="https://www.linkedin.com/in/harshvardhanvatsa/">2024 Harshvardhan Vatsa (hrsvrn)</a>
     </footer>
   )
 }
